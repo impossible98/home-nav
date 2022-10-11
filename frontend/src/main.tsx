518884e0475aa4c8c0c9ed191d54ac784c42e06e
impossible98 <ago2chris@outlook.com>
2022-10-11 23:08:44 +0800

    chore(devDependencies): remove @arco-plugins/vite-react

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 // import third-party modules
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@arco-design/web-react/dist/css/arco.css";
 // import local modules
 import Router from "@/router";
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,11 @@
 import path from "path";
 // import third-party modules
 import react from "@vitejs/plugin-react";
-import vitePluginForArco from "@arco-plugins/vite-react";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    vitePluginForArco({
-      style: "css",
-    }),
   ],
   resolve: {
     alias: {
